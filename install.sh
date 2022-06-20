932c3b3c04e596b0f75a44ad17d12b701696cf85
Joe Collins <j.collins@zengenti.com>
2022-06-20 13:46:04 +0100

    Git config

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Remove existing config files to make way for new
-rm -f ~/.tmux.conf
-rm -f ~/.vimrc
-rm -f ~/.zshrc.pre-oh-my-zsh
+rm -r ~/.gitconfig
+rm -r ~/.gitignore_global
 rm -rf ~/.config/starship.toml
-rm -rf ~/.tmp
-rm -rf ~/.tmuxinator
+rm -f ~/.tmux.conf
 rm -rf ~/.vim
+rm -f ~/.vimrc
 rm -f ~/.zshrc
+rm -f ~/.zshrc.pre-oh-my-zsh
+rm -rf ~/.tmp
 
 # Symlink new config files
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 ln -s ~/dotfiles/starship/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/tmuxinator ~/.tmuxinator
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
